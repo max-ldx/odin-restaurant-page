dc60a5b5b8691856038d5c7466ae1679ce7b94b7
max-ldx <242964282+max-ldx@users.noreply.github.com>
2025-12-31 14:50:26 +0100

    feat: add about page

--- src/about.js
+++ src/about.js
@@ -1,5 +1,13 @@
 export { displayAboutPage };
 
 function displayAboutPage() {
-    console.log('baout')
+    const contentElement = document.querySelector('#content');
+
+    const aboutElement = document.createElement('h1');
+    aboutElement.textContent = 'About';
+    contentElement.appendChild(aboutElement)
+
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = 'This is a simple restaurant website. It is part of The Odin Project curriculum. The goals of this project are to use webpack and reinforce DOM manipulation skills.';
+    contentElement.appendChild(descriptionElement);
 }
